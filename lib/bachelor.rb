624c1fc96fd4cdc94ecc21b2717105a624da9b31
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 16:42:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,12 +7,10 @@ def get_first_name_of_season_winner(data, season)
     if answer == nil  
     info.each do |a|
       a.each do |b|
-        if b.include?("name")
-         step = b[1].split
-         name = step[0]
+        
        
    
-  end 
+  
   end 
   end 
   binding.pry
