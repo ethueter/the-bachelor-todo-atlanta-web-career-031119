fb8fa2e55b9aa064a0247ac8ba433a26c5ff607a
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 17:24:44 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -6,7 +6,6 @@ def get_first_name_of_season_winner(data, season)
     if contestant["status"].include?("Winner")
       contestant.each do |catagory, data|
         if catagory == "name"
-         
         step = data.split
         first_name = step[0]
       end
@@ -17,7 +16,8 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  name = nil 
+  
 end
 
 def count_contestants_by_hometown(data, hometown)
