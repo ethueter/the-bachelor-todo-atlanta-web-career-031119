560b4f4c930fb7bc20e9f0599bf5da0a52d9e6f9
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 17:36:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,7 +29,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  final_number = nil
+  final_number = 0 
   data.each do |season, contestant|
     contestant.each do |catagory|
       if catagory["hometown"] == hometown 
