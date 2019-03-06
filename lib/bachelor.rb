6689b2a5eb4150a3f0e63cf496dec8cb71846884
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 16:31:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def get_first_name_of_season_winner(data, season)
   name = nil 
-  data.each do |season, info|
-    if season == #{season}
+  data.each do |x, info|
+    if x == season 
     info.each do |a|
       a.each do |b|
         if b.include?("name")
