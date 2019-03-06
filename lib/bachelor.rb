7cb9119123305b6b2a07f0509730e8f23cdeabf9
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 16:44:33 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -3,19 +3,14 @@ require 'pry'
 def get_first_name_of_season_winner(data, season)
   answer = nil 
   
-  data["#{season}"].each do |x, info|
-    if answer == nil  
-    info.each do |a|
-      a.each do |b|
+  x = data[season]
         
        
    
   
-  end 
-  end 
+  
   binding.pry
-  end 
-  end
+  
   answer 
 end
 
