6c66746626cc0d20749486bdaed0d1cd20a06846
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-06 18:40:36 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -61,7 +61,7 @@ def get_average_age_for_season(data, season)
   end
   
   final_number = avg_age.reduce(:+) / avg_age.size 
-  final_number.to_i.round 
+  final_number.round 
   binding.pry 
 end
 
